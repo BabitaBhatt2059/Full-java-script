26c4b900970b59478362cb3676be21cf72c247ba
Babita Bhatt <babitabhatta37@gmail.com>
2024-06-02 15:04:00 +0545

    about Operators OF Js

--- First.js
+++ First.js
@@ -1,2 +1,45 @@
-//What is NAN?
-//
+//Console.log is used to log(print) a message to console 
+console.log("Babi");
+//operators and conditional statement
+//use to perform some operations on data
+ //Arithmatic Operators
+ let a=5;
+ let b=10
+ console.log("a+b=",a+b);
+ console.log("a-b=",a-b);
+ console.log("a*b=",a*b);
+ //unary operator
+ console.log("a=",a);
+ console.log("b=",b);
+ //increment decrement
+ console.log("++a=",++a);
+ console.log("--a=",--a);
+ //Assignment operators
+ a*=4;
+ console.log("a=",a);
+ a**=4;
+ console.log("a=",a);
+ //comparasion operator
+ console.log("a==b",a==b);
+ console.log("a!=",a!=b);
+ //logical operators
+ let cond1=a>b;
+ let cond2=a==6;
+ console.log("cond1 && cond2=",cond1&&cond2);
+ 
+
+ 
+ 
+ 
+
+ 
+
+
+ 
+ 
+ 
+ 
+
+ 
+
+
--- index.js
+++
@@ -1,7 +0,0 @@
-console.log('hello World');
-var Name='Babita';
-let caste ="Bhatt"
-console.log('Name')
-console.log(typeof(Name));//to know type of data
-console.log(Name + caste );
-
