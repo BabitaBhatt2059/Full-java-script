2253c258fdf82f03b35ad1d91b2becb834c73ef1
Babita Bhatt <babitabhatta37@gmail.com>
2024-06-04 08:45:59 +0545

    loop in js

---
+++ loop.js
@@ -0,0 +1,35 @@
+//for loop
+for (let i=1; i<=5;i++){
+    console.log("apna college")
+}
+//while loop
+let i=7
+while(i<=5){
+    console.log("apna college");
+    i++;
+}
+//do while loop 
+do {
+    console.log("apna college");
+    i++;
+
+}
+while(i<=10);
+//for of loop
+let str="babita"
+for (let i of str){
+    console.log("i=",i)
+}
+//for in loop
+let student ={
+    name:"babi bhatt",
+    age:20,
+    gpa:3.6,
+    ispass:true
+
+};
+for (let key in student)
+
+{
+console.log(i);
+}
