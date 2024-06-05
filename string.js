8a6adec22228eb4f9f0b1bc4eeca5e27e92ec070
Babita Bhatt <babitabhatta37@gmail.com>
2024-06-05 13:36:25 +0545

    string in js

---
+++ string.js
@@ -0,0 +1,12 @@
+//string is a sequence of characters used to represent text
+//create string
+let str="Apna College";
+console.log(str[8]);
+let specialstring ="this is a templete literal";
+console.log(typeof specialstring);
+let obj = {
+    item: "pen",
+    price:10,
+
+};
+
