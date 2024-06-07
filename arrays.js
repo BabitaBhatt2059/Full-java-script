3cc72c4ee6fd31a022fec89e77fd0f0488a1d0d0
Babita Bhatt <babitabhatta37@gmail.com>
2024-06-07 19:43:40 +0545

    loop in arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+//collection of item
+let markss=[87,90,89,78,86]
+console.log(markss)
+console.log(markss.length)
+//loop in array
+//print all the element in array
+//for loop
+for (let i=0;i<markss.length; i++){
+    console.log(markss[i]);
+
+}
