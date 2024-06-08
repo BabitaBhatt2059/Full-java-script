f772d3c0116477c526c77701b11aec35ecace880
Babita Bhatt <babitabhatta37@gmail.com>
2024-06-08 21:25:21 +0545

    loop

--- arrays.js
+++ arrays.js
@@ -9,3 +9,17 @@ for (let i=0;i<markss.length; i++){
     console.log(markss[i]);
 
 }
+let sum=0;
+let m= prompt("Enter the number 'n' till where you wanna sum of that prime number =");
+for(let i=1; i<=m; i++){
+    sum=sum+i;
+}
+console.log(sum);
+// for loop from n (n-1) (n-2) ... 1
+
+let n=prompt("Enter the number =");
+
+for (let i=n; i>=1; i--){
+    console.log("I am executing "+ i);
+
+}
