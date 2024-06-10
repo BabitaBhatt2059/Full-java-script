05eca7c2e35e681ff18038cda6c699f0c0d503a0
Babita Bhatt <babitabhatta37@gmail.com>
2024-06-10 20:22:16 +0545

    loop

---
+++ dowhileloop.js
@@ -0,0 +1,20 @@
+let c = 0;
+do {
+  count += 1;
+  console.log("I am executing " + c);
+} while (c <= 5);
+//while loop
+const number=prompt("Enter the number =");
+let count=1;
+while(count<10){
+    console.log(number+"*"+count+"="+ number*count);
+    count+=1;
+}
+let co=1; //count starts from 1
+ 
+while (co<15)  //execute the inside commands until condition fails.
+
+ {
+    console.log("I am an engineer"+co);
+    co+=1;
+}
